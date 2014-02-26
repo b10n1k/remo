493ea6f31959c23e92404d99b039cbb4c3a42f77
b10n1k <jbonatakis@gmail.com>
2014-02-27 00:18:26 +0200

    [bug 861993] Add progress bar for votes fix Jenkins failures add a descriptive text on the right of the meter bar remove tab indentation, add font-family and weight to meter_text remove formatdecimal function, add get_meter_value to handle Zero division and output format

--- remo/voting/helpers.py
+++ remo/voting/helpers.py
@@ -8,8 +8,22 @@ def get_users_voted(poll):
 
     return poll.users_voted.all().count()
 
+
 @register.filter
 def get_voters_per_group(poll):
     """Return the number of users of a specific poll per group."""
 
-    return User.objects.filter(groups=poll.valid_groups).count() 
+    return User.objects.filter(groups=poll.valid_groups).count()
+
+
+@register.filter
+def get_meter_value(poll):
+    """Return the actual value for the meter element."""
+
+    users_voted = float(get_users_voted(poll))
+    voters_group = float(get_voters_per_group(poll))
+    try:
+        val = (users_voted/voters_group)*100
+        return int(val)
+    except ZeroDivisionError:
+        return 0
